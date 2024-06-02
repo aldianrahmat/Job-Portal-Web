ae73e56736a71cbc520859359ba8b4de01693802
dedeadika30d <ddadkd30@gmail.com>
2024-06-02 23:03:21 +0700

    new

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { FiSearch, FiMap } from "react-icons/fi";
 
-const Banner = ({ query, handleInputChange, location, handleLocationChange }) => {
+const Banner = ({ query, handleInputChange, location, handleLocationChange, handleSearch }) => {
+    const onSubmit = (event) => {
+        event.preventDefault();
+        handleSearch();
+    };
+
     return (
-        <div className='max-w-screen-2x1 container mx-auto xl:px-24 px-24 md:py-20 py-14 '>
+        <div className='max-w-screen-2x1 container mx-auto xl:px-24 px-24 md:py-20 py-14'>
             <h1 className='text-5xl font-bold text-primary mb-3'>
                 Temukan <span className='text-blue'>pekerjaan baru</span> Anda hari ini
             </h1>
@@ -11,7 +16,7 @@ const Banner = ({ query, handleInputChange, location, handleLocationChange }) =>
                 Ribuan pekerjaan di sektor komputer, teknik, dan teknologi menanti Anda
             </p>
 
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
                     <div className='flex md:rounded-md rounded shadow-sm ring-1 ring-inset  
                     focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full'>
@@ -36,19 +41,19 @@ const Banner = ({ query, handleInputChange, location, handleLocationChange }) =>
                             placeholder='Lokasi'
                             className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900
                             placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6' 
-                            onChange={handleLocationChange}  // Tambahkan handler untuk lokasi
+                            onChange={handleLocationChange}
                             value={location} 
                         />
                         <FiMap className='absolute mt-2.5 ml-2 text-gray-400' />
                     </div>
 
-                    <button type='submit' className='bg-blue py-2 px-8 text-white md:rounded-none rounded'>
+                    <button type='submit' className='bg-blue py-2 px-8 text-white md:rounded-none rounded mt-6 md:mt-0 ml-4'>
                         Cari
                     </button>
                 </div>
             </form>
         </div>
     );
-}
+};
 
 export default Banner;
