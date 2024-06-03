55bfc6fd1efa6e6a86088373f8d7582fcfe8f5b5
dedeadika30d <ddadkd30@gmail.com>
2024-06-03 15:44:29 +0700

    new

--- src/Pages/MyJobs.jsx
+++ src/Pages/MyJobs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiSearch, FiMap } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
 
 const MyJobs = () => {
     const [jobs, setJobs] = useState([]);
@@ -69,18 +69,20 @@ const MyJobs = () => {
         <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
             <div className="my-jobs-container">
                 <h1 className="text-center p-4">Semua Pekerjaan Saya</h1>
-                <div className="search-box p-2 text-center mb-2">
-                    <input
-                        onChange={(e) => setSearchText(e.target.value)}
-                        type="text"
-                        name="title"
-                        id="job-title"
-                        placeholder='Cari'
-                        className="py-2 pl-3 border focus:outline-none lg:w-6/12 mb-4 w-full"/>
-                        <FiSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400" />
-
+                <div className="relative p-2 text-center mb-2">
+                    <div className="relative inline-block lg:w-6/12 w-full">
+                        <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                        <input
+                            onChange={(e) => setSearchText(e.target.value)}
+                            type="text"
+                            name="title"
+                            id="job-title"
+                            placeholder='Cari'
+                            className="py-2 pl-10 pr-3 border focus:outline-none w-full"
+                        />
+                    </div>
                     <button
-                        className="bg-blue text-white font-semibold px-8 py-2 rounded-sm mb-4"
+                        className="bg-blue text-white font-semibold px-8 py-2 rounded-sm mb-4 ml-4"
                         onClick={handleSearch}>Cari</button>
                 </div>
             </div>
@@ -161,12 +163,17 @@ const MyJobs = () => {
                                                     <div className="relative inline-block text-left">
                                                         <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
                                                             <span className="mr-1">Aksi</span>
-                                                            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fillRule="evenodd" d="M3 7a1 1 0 011-1h1V5c0-1.1.9-2 2-2h6a2 2 0 012 2v1h1a1 1 0 110 2H4a1 1 0 01-1-1zm12 2V18a2 2 0 01-2 2H7a2 2 0 01-2-2v-9h10zm-4 0h2v9H9V9zm-3-2V5h6v2H6z" clipRule="evenodd"/></svg>
-                                                            <ul className="absolute hidden text-gray-700 pt-1 group-hover:block">
-                                                                <li><button className="rounded-t w-full py-2 px-4 block whitespace-no-wrap bg-gray-200 hover:bg-gray-400">Ubah</button></li>
-                                                                <li><button onClick={() => deleteJob(job.id)} className="rounded-b w-full py-2 px-4 block whitespace-no-wrap bg-gray-200 hover:bg-gray-400">Hapus</button></li>
-                                                            </ul>
+                                                            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fillRule="evenodd" d="M3 7a1 1 0 011-1h1V5c0-1.1.9-2 2-2h6a2 2 0 012 2v1h1a1 1 0 011 1v2a1 1 0 01-1 1v7a2 2 0 01-2 2H6a2 2 0 01-2-2V10a1 1 0 01-1-1V7zm2-1v10a1 1 0 001 1h8a1 1 0 001-1V7H5zm5-4a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" /></svg>
                                                         </button>
+                                                        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                                            <div className="py-1">
+                                                                <button
+                                                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                                                    onClick={() => deleteJob(job.id)}>
+                                                                    Hapus
+                                                                </button>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </td>
                                             </tr>
@@ -176,16 +183,22 @@ const MyJobs = () => {
                             </table>
                         </div>
                     </div>
-                </div>
 
-                {/* Pagination */}
-                <div className="flex justify-center text-black space-x-8 mb-8">
-                    {currentPage > 1 && (
-                        <button className="hover:underline" onClick={prevPage}>Sebelumnya</button>
-                    )}
-                    {indexOfLastItem < jobs.length && (
-                        <button onClick={nextPage} className="hover:underline">Selanjutnya</button>
-                    )}
+                    {/* Pagination */}
+                    <div className="mt-4 flex justify-center">
+                        <button
+                            className="px-4 py-2 mx-1 bg-gray-200 text-gray-700 rounded-md"
+                            onClick={prevPage}
+                            disabled={currentPage === 1}>
+                            Previous
+                        </button>
+                        <button
+                            className="px-4 py-2 mx-1 bg-gray-200 text-gray-700 rounded-md"
+                            onClick={nextPage}
+                            disabled={indexOfLastItem >= jobs.length}>
+                            Next
+                        </button>
+                    </div>
                 </div>
             </section>
         </div>
