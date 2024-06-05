aa3a80b24e40510c966348a13e1392d56868fe38
aliccciao <alicia.utomo@gmail.com>
2024-06-05 07:11:18 +0700

    feat: add naila & samuel's work

---
+++ src/Pages/JobDetails.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const JobDetails = () => {
+  return (
+    <div>
+      <h1>Tentang Pekerjaan</h1>
+      <p>
+        PT. Norojono Tobacco International, Jakarta Timur, Jakarta Raya,
+        Indonesia
+      </p>
+      <h2>Front End Developer</h2>
+      <button>Melamar Sekarang</button>
+
+      <div>
+        <h3>Manfaat</h3>
+        <ul>
+          <li>Gaji pokok UMR</li>
+          <li>Asuransi jiwa</li>
+          <li>Employee diskon</li>
+          <li>Asuransi kesehatan</li>
+          <li>Asuransi pensiun</li>
+          <li>Asuransi tenaga kerja</li>
+          <li>Cuti tahunan</li>
+          <li>Tunjangan hari raya</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Job Deskripsi</h3>
+        <ul>
+          <li>Memahami dan mampu mengembangkan Front End Web Developer</li>
+          <li>
+            Mengidentifikasi dan menangani troubleshooting teknis selama proses
+            pengembangan aplikasi
+          </li>
+          <li>
+            Membuat aplikasi internal ataupun eksternal (custom) dalam platform
+            web sesuai design dan timeline yang disepakati
+          </li>
+          <li>
+            Melakukan koordinasi untuk memastikan kesesuaian dengan pemangku
+            kepentingan bisnis, manajemen, dan tim lainnya
+          </li>
+          <li>Mampu menyajikan laporan reguler terkait progress proyek</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Kualifikasi</h3>
+        <ul>
+          <li>
+            Minimal pendidikan S1 Jurusan Teknik Informatika, Ilmu Komputer,
+            Teknik Industri akan menjadi nilai tambah
+          </li>
+          <li>Memiliki pengalaman 2 Tahun sebagai Web Developer</li>
+          <li>
+            Memahami database & programming language & API Integration
+            menggunakan JavaScript/PHP/Golang, HTML, Java, CSS
+          </li>
+          <li>Memahami versioning tools (such as Git, Mercurial or SVN)</li>
+          <li>Mau belajar dan beradaptasi dengan hal baru</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>PROFILE:</h3>
+        <p>
+          Nojorono Kudus, merupakan salah satu perusahaan pelopor rokok kretek
+          di Indonesia. Nojorono *baca: No-Yo-Ro-No didirikan pada 14 oktober
+          1932 oleh Ko Djee Siong dan Tan Djing Thay dan berpusat di Kota Kudus,
+          Jawa Tengah. Nojorono Kudus yang dikenal sebagai pemilik merek dagang
+          Minak Djinggo dan Clas Mild, saat ini menduduki posisi kelima dalam
+          industri rokok terbesar di Indonesia.
+        </p>
+      </div>
+
+      <div>
+        <h3>VISI:</h3>
+        <p>
+          1. Menjadi salah satu Perusahaan yang "Best Managed" di industri, yang
+          memiliki tanggung jawab sosial dan kesadaran pada lingkungan.
+        </p>
+      </div>
+
+      <div>
+        <h3>MISI:</h3>
+        <p>
+          1. Menciptakan nilai bagi seluruh pemangku kepentingan (Stakeholders),
+          melalui pertumbuhan dan Profitabilitas yang berkelanjutan, dalam
+          ikatan kekeluargaan yang erat dan bermartabat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default JobDetails;
